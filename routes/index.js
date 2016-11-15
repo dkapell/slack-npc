d2373c5d3c0728fb4e1a2c09affbc355db651b59
Dave Kapell <dave.kapell@delphix.com>
2016-11-15 17:20:48 -0500

    new node version

---
+++ routes/index.js
@@ -0,0 +1,57 @@
+'use strict';
+var express = require('express');
+var Slack = require('node-slackr');
+var _ = require('underscore');
+var fs = require('fs');
+
+var router = express.Router();
+
+var TOKEN = process.env.INCOMING_SLACK_TOKEN;
+var slack = new Slack(process.env.INCOMING_SLACK_WEBHOOK);
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+    var text = req.body.text;
+    var token = req.body.token;
+    var user_name = req.body.user_name;
+
+    if (token !== TOKEN){
+        return req.json({
+            "response_type": "ephemeral",
+            "text": "Invalid Command Token"
+        });
+    }
+
+    var parts = text.match(/([@#][^\s]+)\s+(['"]?)(.+?)\2\s+(.+))/);
+    if (! parts){
+        return req.json({
+            "response_type": "ephemeral",
+            "text": "Failed; message should be like: /npc #channel NPCName hey what's up?"
+        });
+    }
+
+    var channel = parts[1];
+    var user = parts[3];
+    var message = parts[4];
+
+    slack.notify({
+        text: message,
+        username: user,
+        channel: channel
+    }, function(err){
+        if (err){
+           return req.json({
+                "response_type": "ephemeral",
+                "text": "Error sending message: " + err
+            });
+       } else {
+            return req.json({
+                "response_type": "ephemeral",
+                "text": "Sent message to " + channel + ' as ' + user
+            });
+       }
+
+    });
+});
+
+module.exports = router;
